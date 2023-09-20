e7489dc8c05b870d6e6a777a481c20b9a2de3d08
unknown <xuanpham3623@gmail.com>
2023-09-20 23:32:34 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "welcome yo contact book application."})
+});
+
+module.exports = app;
